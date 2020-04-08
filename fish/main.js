04ad2960af6fce6580fa326f31d2c1f8e9714033
Connor Milligan <connoramilligan@gmail.com>
2020-04-07 20:15:51 -0400

    Got basic functionalliy and added 9 more fish

--- fish/main.js
+++ fish/main.js
@@ -8,37 +8,47 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const fish = require('./fish.json');
+const prefix = '!';
 
-const embed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+var embed = new Discord.MessageEmbed()
+	
 
 client.on('ready', () => {
     console.log('I am ready!');
-    console.log(fish);
 });
 
 //Processor for all the messages
 client.on('message', message => {
+	if ((!message.content.startsWith(prefix) || message.author.bot) && message.channel.type != 'dm') return;
 
-    if (message.content === '!test' && message.channel.type != 'dm') {
-        message.channel.send(fish[0].name);
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+    if (command === 'fish') {
+		if (args.length != 1) {
+			buildFishEmbed(fish.fish[0]);
+		} else {
+			buildFishEmbed(fish.fish[Number(args[0])]);
+		}
+        message.channel.send(embed);
     }
 });
 
+function buildFishEmbed(fish) {
+	embed = new Discord.MessageEmbed().setTitle('You caught a ' + fish.name + '!')
+	.setDescription(fish.catch)
+	.setImage(fish.image)
+	.addFields(
+		{ name: 'Value', value: fish.price + ' bells', inline: true },
+		{ name: 'Size', value: fishSize(fish) + ' cm', inline: true },
+		{ name: 'Rarity', value: fish.rarity, inline: true },
+	)
+	.setTimestamp();
+}
+
+function fishSize(fish) {
+	return ((Math.random() * (fish.size[0] - fish.size[1]) + fish.size[1]).toFixed(1));
+}
+
 //DO NOT FORGET TO REMOVE LOGIN CODE BEFORE UPLOAD
-client.login('na');
+client.login('MzgwNTA5NDcwMTgwMjQ1NTA1.Xo0E0A.r7B1kncdTCPOr8riBbbL0XoIjdk');
